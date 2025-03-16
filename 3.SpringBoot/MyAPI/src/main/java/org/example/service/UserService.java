1e84aab4310770460e74e40acff075ab12a63196
novakvova <novakvova@gmail>
2025-03-16 17:53:48 +0200

    Google auth with Java

--- 3.SpringBoot/MyAPI/src/main/java/org/example/service/UserService.java
+++ 3.SpringBoot/MyAPI/src/main/java/org/example/service/UserService.java
@@ -1,13 +1,25 @@
 package org.example.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.example.config.security.JwtService;
 import org.example.dto.user.UserAuthDto;
 import org.example.dto.user.UserRegisterDto;
 import org.example.entities.UserEntity;
 import org.example.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +28,9 @@ public class UserService {
     private final IUserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+
+    @Value("${google.api.userinfo}")
+    private String googleUserInfoUrl;
 
     // Реєстрація нового користувача
     public void registerUser(UserRegisterDto dto) {
@@ -40,4 +55,43 @@ public class UserService {
         // Генерація JWT токену
         return jwtService.generateAccessToken(foundUser);
     }
+
+    public String signInGoogle(String access_token) throws IOException {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + access_token);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        ResponseEntity<String> response = restTemplate.exchange(googleUserInfoUrl, HttpMethod.GET, entity, String.class);
+        if (response.getStatusCode().is2xxSuccessful()) {
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, String> userInfo = mapper.readValue(response.getBody(), new TypeReference<Map<String, String>>() {});
+            var userEntity = userRepository.findByUsername(userInfo.get("email"))
+                    .orElse(null); // Порожній об'єкт
+            if(userEntity == null){
+                userEntity=new UserEntity();
+                userEntity.setUsername(userInfo.get("email"));
+                userEntity.setPassword("");
+                userRepository.save(userEntity);
+//                user = new User(
+//                        0L,
+//                        userInfo.get("email"),
+//                        " ",
+//                        userInfo.get("given_name"),
+//                        userInfo.get("family_name"),
+//                        userInfo.get("email"),
+//                        new Date(),
+//                        storageService.saveImage(userInfo.get("picture"), FileFormats.WEBP),
+//                        true,true,true,true,
+//                        Set.of(),
+//                        Set.of(userRole),
+//                        Set.of()
+//                );
+//                user = userRepo.saveAndFlush(user);
+            }
+            var jwt = jwtService.generateAccessToken(userEntity);
+            return jwt;
+        }
+        return  null;
+    }
+
 }
